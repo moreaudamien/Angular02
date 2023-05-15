258fcc8f1b6808ccd629e0cb8a4968c813692e83
moreaudamien <moreau_student2023@wilder.school>
2023-05-15 12:22:10 +0200

    2eme partie

--- angular2/src/app/user-profile/user-profile.component.ts
+++ angular2/src/app/user-profile/user-profile.component.ts
@@ -1,11 +1,53 @@
 import { Component } from '@angular/core';
 
+
+
+export class User {
+  name: string;
+  firstName: string;
+  age: number;
+  isAgeHidden: boolean;
+  quote: string;
+  photo: string;
+
+constructor(
+name: string,
+firstName: string,
+age: number,
+isAgeHidden: boolean,
+quote: string,
+photo: string 
+){
+this.name = name;
+this.firstName = firstName;
+this.age = age;
+this.isAgeHidden = isAgeHidden;
+this.quote = quote;
+this.photo = photo;}
+}
 @Component({
   selector: 'app-user-profile',
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css']
 })
+
 export class UserProfileComponent {
+  user: User; 
+
+constructor() { 
+  this.user = new User(
+  'dowie',
+  'john',
+  25,
+  false,
+  '',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPKf5N78lFn50X--npJiERGl_fUzSNb4QvWQ&usqp=CAU',
+  )
+}
+
+
+
+/*export class UserProfileComponent {
 
   user = {
     name : 'Dowie',
@@ -14,7 +56,7 @@ export class UserProfileComponent {
     isAgeHidden : false,
     quote : '',
     photo : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTPKf5N78lFn50X--npJiERGl_fUzSNb4QvWQ&usqp=CAU'
-  };
+  };*/
 
 ageVisibility(): void {
   this.user.isAgeHidden = !this.user.isAgeHidden;
